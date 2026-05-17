d5dc2f217e971ab0df896f953631cdff36e0d143
ugnemi <ugnemi@kth.se>
2026-05-17 23:12:32 +0200

    Add descriptions; adjust styles and formatting

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 library(shiny)
 source("./helpers.R")
 
-parameter_tabs <- tabsetPanel(
+parameterTabs <- tabsetPanel(
   id = "params",
   type = "hidden",
   tabPanel("weibull",
@@ -13,8 +13,8 @@ parameter_tabs <- tabsetPanel(
   )
 )
 
-ic_tabs <- tabsetPanel(
-  id = "ic_width",
+icTabs <- tabsetPanel(
+  id = "ic.width",
   type = "hidden",
   tabPanel("No"),
   tabPanel("Yes", 
@@ -22,25 +22,56 @@ ic_tabs <- tabsetPanel(
                         min = 1, max = 1000))
 )
 
-rc_tabs <- tabsetPanel(
-  id = "rc_day",
+rcTabs <- tabsetPanel(
+  id = "rc.day",
   type = "hidden",
   tabPanel("No"),
   tabPanel("Yes",
            numericInput("followup", "Censor after day:", 365, min = 365))
 )
 
+style.str <- "
+h2 {
+    font-family: Avenir; 
+    padding: 10px 5px 15px; 
+    background-color: hsl(0, 0%, 94%);
+}
+
+h4 {
+    font-family: Avenir;
+}
+
+p {
+    font-family: Avenir;
+}
+
+.pdd {
+    padding: 10px;
+}
+
+.section {
+    background-color: hsl(0, 0%, 94%);
+    padding: 10px;
+}
+"
+
 
 ui <- fluidPage(
-  tags$style("h2 {text-align: center; font-family: Helvetica; padding: 10px 5px 15px; background-color: hsl(0, 0%, 94%);}"),
+  tags$style(style.str),
   titlePanel("Coarsening and interval-censoring in time-to-event data"),
+  div(
+    p("This application illustrates model bias for different parametric and non-parametric methods for analyzing interval-censored and coarsened data.")
+  ),
+  hr(),
   sidebarLayout(
     # Sidebar for choosing the parameters
     sidebarPanel(
+      h4("Simulation parameters"),
+      hr(),
       # For visualization
       numericInput("N", "Observations", 2000, min = 1, max = 5000),
       selectInput("dist", "Distribution", list("weibull")),
-      parameter_tabs,
+      parameterTabs,
       actionButton("viz", "Set parameters"),
       p(br()),
       
@@ -52,10 +83,10 @@ ui <- fluidPage(
         list("Rounded up", "Rounded down")
       ),
       selectInput("cens", "Interval-censored?", list("No", "Yes")),
-      ic_tabs,
+      icTabs,
       selectInput("rc", "Right-censored?", list("No", "Yes")),
-      rc_tabs,
-      numericInput("n.iter", "Number of iterations", 1000, min = 1, max = 5000),
+      rcTabs,
+      numericInput("n.iter", "Number of iterations", 200, min = 1, max = 5000),
       radioButtons(
         inputId = "days", 
         label = "Evaluate at:", 
@@ -67,22 +98,42 @@ ui <- fluidPage(
     ),
     # Main panel with plots
     mainPanel(
-      htmlOutput("instructions"),
-      plotOutput("survival"),
-      plotOutput("surv.bias"),
-      tableOutput("par.bias")
+      div(
+        h4("Instructions:"),
+        HTML("<ol>
+               <li>Press \"Set parameters\" to fix the parameters and display the distribution.</li>
+               <li>Press \"Evaluate bias\" to calculate the bias of survival estimates and distribution parameters. If the number of iterations is large (>500), the plots may take a while to load.</li>
+            </ol>"),
+        style = "border: solid; border-radius: 0px; padding: 10px; font-family: Monospace; "
+      ),
+      h4("Plot of the survival function for the distribution", class = "section"),
+      div(
+        textOutput("surv.desc"),
+        plotOutput("survival"),
+        class = "pdd"
+      ),
+      h4("Bias evaluation", class = "section"),
+      div(
+        textOutput("tbl.desc"),
+        div(tableOutput("par.bias"), style = "padding: 10px 5px 0px"),
+        class = "pdd"
+      ),
+      div(
+        htmlOutput("plt.desc"),
+        plotOutput("surv.bias"),
+        class = "pdd"
       )
+    )
+  ),
+  div(
+    class = "footer",
+    hr(),
+    HTML("<footer>Created by Ugne Milasiunaite, last updated on 17/05/2026: <a href=\"https://github.com/ugmi/ShinyThesis\">source code</a>.</footer>"),
+    style = "padding: 10px; text-align: right; font-family: Avenir;"
   )
 )
 
 server <- function(input, output, session) {
-  # Display instructions
-  output$instructions <- renderUI({
-    HTML("Press \"Visualize\" to display the distribution.<br/>
-    Press \"Evaluate Bias\" to plot the bias of survival estimates and distribution parameters.<br/>
-    If the number of iterations is large, the plots may take a while to load.")
-  })
-  
   # Update the parameter tabs user sees based on their choice of distribution
   observeEvent(input$dist, {
     updateTabsetPanel(inputId = "params", selected = input$dist)
@@ -90,23 +141,33 @@ server <- function(input, output, session) {
   
   # Only show the choice for the censoring interval width if interval-censored
   observeEvent(input$cens, {
-    updateTabsetPanel(inputId = "ic_width", selected = input$cens)
+    updateTabsetPanel(inputId = "ic.width", selected = input$cens)
   })
   
   # Only show the choice for follow-up length if right-censored
   observeEvent(input$rc, {
-    updateTabsetPanel(inputId = "rc_day", selected = input$rc)
+    updateTabsetPanel(inputId = "rc.day", selected = input$rc)
   })
   
   # Plot the survival curve when the user presses the "Visualize" button
-  surv_prob <- eventReactive(input$viz, {
+  surv.prob <- eventReactive(input$viz, {
     switch(input$dist,
            "weibull" = 1 - pweibull(1:2000, input$shape, input$scale),
            "exponential" = 1 - pexp(1:2000, input$rate))
   })
   
+  txt.surv <- eventReactive(input$viz, {
+    paste0(
+      "The plot below displays the survival function for Weibull distribution ",
+      "with shape parameter ", input$shape, 
+      " and scale parameter ", input$scale, "."
+    )
+  })
+  
+  output$surv.desc <- renderText({ txt.surv() })
+  
   output$survival <- renderPlot({
-    surv <- surv_prob()
+    surv <- surv.prob()
     plot(1:2000, surv, xlim = c(0, 1826), pch = 20, cex = 0.5, xaxt = "n",
          xlab = "", ylab = "Survival probability", main = "Survival curve",
          ylim = c(0, 1))
@@ -153,6 +214,45 @@ server <- function(input, output, session) {
            "weibull" = 1 - pweibull(days(), input$shape, input$scale),
            "exponential" = 1 - pexp(days(), input$rate))
     })
+  
+  txt.tbl <- eventReactive(input$click, {
+    paste0(
+      "The table below shows the parameter bias over ", 
+      input$n.iter, " iterations. ", 
+      "For each iteration, we simulated ", input$N, 
+      " survival times distributed according to the ",
+      "Weibull distribution with shape parameter ", input$shape, 
+      " and scale parameter ", input$scale, 
+      ", with coarsening fixed at 1 day ",
+      "(rounded ", substring(input$round, 8), "). ",
+      "The censoring interval width is ", 
+      if(input$cens == "Yes") input$width else input$coar, " day."
+    )
+  })
+  
+  output$tbl.desc <- renderText({ txt.tbl() })
+  
+  output$par.bias <- renderTable({
+    par.bias <- average_bias(estimates()[["pars"]], df.pars()[["pars"]])
+    par.bias
+  }, rownames = TRUE, align = "c", digits = 5, hover = TRUE, width = "100%", bordered = TRUE)
+  
+  txt.plt <- eventReactive(input$click, {
+    paste0(
+      "The plot below shows the bias averaged over ", input$n.iter, 
+      " simulations performed as described above, for six different models:",
+      "<ol>
+          <li>KM.MID: Kaplan-Meier with midpoint imputation,</li>
+          <li>KM.IGN: Kaplan-Meier with upper limit of the censoring interval treated as the observed event time, i.e., the censoring is ignored,</li>
+          <li>NPMLE: Turnbull estimate,</li>
+          <li>MID: Weibull parametric model with midpoint imputation,</li>
+          <li>IGN: Weibull parametric model ignoring the censoring,</li>
+          <li>IC: Weibull parametric model accounting for interval-censoring.</li>
+      </ol>"
+    )
+  })
+  
+  output$plt.desc <- renderUI({ HTML(txt.plt()) })
 
   output$surv.bias <- renderPlot({
     prob.bias <- average_bias(estimates()[["prob"]], P())
@@ -160,11 +260,7 @@ server <- function(input, output, session) {
               c("darkorange", "red", "brown", "forestgreen", "royalblue", "cyan3"),
               paste0("Bias for est. S(t), \ninterval width = ", df.pars()[["width"]], " days"))
   })
-
-  output$par.bias <- renderTable({
-    par.bias <- average_bias(estimates()[["pars"]], df.pars()[["pars"]])
-    par.bias
-  }, rownames = TRUE, align = "c", digits = 5)
+  
 }
 
 shinyApp(ui, server)
